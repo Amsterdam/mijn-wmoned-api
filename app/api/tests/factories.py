67b000f823ab2423c3b54968d3b2aeef3600c14c
Johan Otten <drakonen@drakonen.com>
2019-04-24 14:31:48 +0200

    Bring tests up to date with other projects

--- app/api/tests/factories.py
+++ app/api/tests/factories.py
@@ -1,6 +1,6 @@
 import datetime
 from faker import Faker
-from factory import fuzzy, List,  SubFactory, DictFactory
+from factory import fuzzy, List, SubFactory, DictFactory
 fake = Faker('nl_NL')
 
 
@@ -12,7 +12,7 @@ class PGBFactory(DictFactory):
 
 
 class VoorzieningFactory(DictFactory):
-    omschrijving = fake.sentence()  # pylint: disable=no-member
+    omschrijving = fake.sentence()
     Wet = fuzzy.FuzzyInteger(0, 3)
     Actueel = fuzzy.FuzzyChoice([True, False])
     Startdatum = fuzzy.FuzzyDate(datetime.date(2008, 1, 1)).__str__()
@@ -20,8 +20,8 @@ class VoorzieningFactory(DictFactory):
     Volume = fuzzy.FuzzyInteger(0, 100)
     Eenheid = fuzzy.FuzzyChoice(['01', '04', '14', '16', '82', '83'])
     Frequentie = fuzzy.FuzzyChoice([1, 2, 3, 4, 5, 6])
-    Omvang = fake.name()  # pylint: disable=no-member
-    Leverancier = fake.name()  # pylint: disable=no-member
+    Omvang = fake.name()
+    Leverancier = fake.name()
     Checkdatum = fuzzy.FuzzyDate(datetime.date(2008, 1, 1)).__str__()
     PGBbudget = List([SubFactory(PGBFactory, year=year) for year
                       in [2016, 2017, 2018]])
