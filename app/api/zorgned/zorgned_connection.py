e47e29ac98dd28b44fa2bcb4b10f54854985842b
Johan Otten <drakonen@drakonen.com>
2020-09-23 11:20:36 +0200

    syntax error

--- app/api/zorgned/zorgned_connection.py
+++ app/api/zorgned/zorgned_connection.py
@@ -11,6 +11,6 @@ class ZorgNedConnection:
 
     def get_voorzieningen(self, bsn):
         """ Get voorzieningen from ZorgNed based on a BSN """
-        res = requests.get(f"{API_URL}/getvoorzieningen?token{quote(credentials['API_KEY'])}&bsn={bsn}")
+        res = requests.get(f"{API_URL}/getvoorzieningen?token={quote(credentials['API_KEY'])}&bsn={bsn}")
 
         return (res.status_code, json.loads(res.text))
