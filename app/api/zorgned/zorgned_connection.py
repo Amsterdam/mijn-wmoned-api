c93740d81bb2e722dfef4ba628de1a663b7b7930
Johan Otten <drakonen@drakonen.com>
2020-09-23 13:34:14 +0200

    syntax error

--- app/api/zorgned/zorgned_connection.py
+++ app/api/zorgned/zorgned_connection.py
@@ -1,9 +1,12 @@
 from urllib.parse import quote
+import logging
 
 import requests
 import json
 
 from .config import credentials, API_URL
+
+logger = logging.getLogger(__name__)
 
 
 class ZorgNedConnection:
@@ -11,6 +14,7 @@ class ZorgNedConnection:
 
     def get_voorzieningen(self, bsn):
         """ Get voorzieningen from ZorgNed based on a BSN """
+        logging.error(f"requesting from {API_URL}/getvoorzieningen?token=key&bsn=bsn")
         res = requests.get(f"{API_URL}/getvoorzieningen?token={quote(credentials['API_KEY'])}&bsn={bsn}")
 
         return (res.status_code, json.loads(res.text))
