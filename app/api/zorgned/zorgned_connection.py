ae42563022ba35721e1b6f410788a8e55a9f650f
Johan Otten <drakonen@drakonen.com>
2020-09-24 09:19:58 +0200

    added debug logging

--- app/api/zorgned/zorgned_connection.py
+++ app/api/zorgned/zorgned_connection.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from urllib.parse import quote
 import logging
 
@@ -8,13 +9,23 @@ from .config import credentials, API_URL
 
 logger = logging.getLogger(__name__)
 
+log_raw = False
+
 
 class ZorgNedConnection:
     """ This class represents the connection to the ZorgNed API """
 
     def get_voorzieningen(self, bsn):
         """ Get voorzieningen from ZorgNed based on a BSN """
-        logging.error(f"requesting from {API_URL}/getvoorzieningen?token=key&bsn=bsn")
-        res = requests.get(f"{API_URL}/getvoorzieningen?token={quote(credentials['API_KEY'])}&bsn={bsn}")
+        url = f"{API_URL}/getvoorzieningen?token={quote(credentials['API_KEY'])}&bsn={bsn}"
+        if log_raw:
+            print(f"requesting from: {url}")
+
+        res = requests.get(url)
+
+        if log_raw:
+            print("\n\n", res.text, "\n\n")
+            pprint(json.loads(res.text))
+            print("Status:", res.status_code)
 
         return (res.status_code, json.loads(res.text))
