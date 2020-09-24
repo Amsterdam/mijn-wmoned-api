e03cd983df02b51a586484cd8de8d25b1da02c9e
Johan Otten <drakonen@drakonen.com>
2020-09-24 09:38:33 +0200

    this is the import

--- app/api/scripts/get_raw.py
+++ app/api/scripts/get_raw.py
@@ -2,11 +2,11 @@ from sys import argv
 
 from api.zorgned.zorgned_connection import ZorgNedConnection
 
-import app.api.zorgned.zorgned_connection
+import api.zorgned.zorgned_connection
 
 
 bsn = argv[1]
-app.api.zorgned.zorgned_connection.log_raw = True
+api.zorgned.zorgned_connection.log_raw = True
 
 con = ZorgNedConnection()
 status, response = con.get_voorzieningen(bsn)
