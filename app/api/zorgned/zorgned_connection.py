b3cc775e8728fd783b2ae86a9d2a197c8917e3c0
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-09-09 10:02:40 +0200

    Add v2 connection

--- app/api/zorgned/zorgned_connection.py
+++ app/api/zorgned/zorgned_connection.py
@@ -5,7 +5,7 @@ import logging
 import requests
 import json
 
-from .config import credentials, API_URL
+from .config import API_TOKEN_V2, credentials, API_URL, API_URL_V2, GEMEENTE_CODE
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +22,27 @@ class ZorgNedConnection:
             print(f"requesting from: {url}")
 
         res = requests.get(url, timeout=9)
+
+        if log_raw:
+            print("\n\n", res.text, "\n\n")
+            pprint(json.loads(res.text))
+            print("Status:", res.status_code)
+
+        return (res.status_code, json.loads(res.text))
+
+    def get_voorzieningen_v2(self, bsn, merge_headers):
+        """Get voorzieningen from ZorgNed based on a BSN"""
+        url = f"{API_URL_V2}/gemeenten/{GEMEENTE_CODE}/ingeschrevenpersonen/{bsn}/aanvragen"
+
+        if log_raw:
+            print(f"requesting from: {url}")
+
+        headers = {"Authorization": "Token %s" % API_TOKEN_V2}
+        headers.update(merge_headers)
+
+        print("\n\n", headers, "\n\n")
+
+        res = requests.get(url, timeout=9, headers=headers)
 
         if log_raw:
             print("\n\n", res.text, "\n\n")
