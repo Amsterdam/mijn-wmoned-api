0c5e352dfd17cdb95772e2ae292d587b707f3daa
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-09-09 10:02:40 +0200

    Add v2 connection

--- app/api/zorgned/config.py
+++ app/api/zorgned/config.py
@@ -7,7 +7,13 @@ def check_env():
     """
     missing_vars = [
         v
-        for v in ["WMO_NED_API_KEY", "WMO_NED_API_URL", "TMA_CERTIFICATE"]
+        for v in [
+            "WMO_NED_API_KEY",
+            "WMO_NED_API_URL",
+            "TMA_CERTIFICATE",
+            "WMO_NED_API_URL_V2",
+            "WMO_NED_API_TOKEN",
+        ]
         if not os.getenv(v, None)
     ]
     if missing_vars:
@@ -16,8 +22,13 @@ def check_env():
         )
 
 
-credentials = dict(API_KEY=os.getenv("WMO_NED_API_KEY").strip())
+credentials = dict(API_KEY=os.getenv("WMO_NED_API_KEY", "").strip())
 
 SENTRY_DSN = os.getenv("SENTRY_DSN", None)
 
-API_URL = os.getenv("WMO_NED_API_URL").strip()
+API_URL = os.getenv("WMO_NED_API_URL", "").strip()
+
+
+API_URL_V2 = os.getenv("WMO_NED_API_URL_V2", "").strip()
+API_TOKEN_V2 = os.getenv("WMO_NED_API_TOKEN", "").strip()
+GEMEENTE_CODE = "0363"
