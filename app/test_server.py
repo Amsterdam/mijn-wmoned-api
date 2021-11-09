775659489dc1a06d5ac65f1f7b7638398fa29e92
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-11-09 09:36:05 +0100

    Add test data, description, script

--- app/test_server.py
+++ app/test_server.py
@@ -20,7 +20,127 @@ class ZorgnedApiMock:
     status_code = 200
 
     def json(self):
-        return {"_embedded": {"aanvraag": []}}
+        return {
+            "_embedded": {
+                "aanvraag": [
+                    {
+                        "_links": None,
+                        "identificatie": "123123",
+                        "regeling": {"identificatie": "WMO", "omschrijving": "WMO"},
+                        "datumAanmelding": "2012-11-30",
+                        "datumAanvraag": "2012-11-30",
+                        "beschikking": {
+                            "beschikkingNummer": 123123123,
+                            "datumAfgifte": "2012-11-30",
+                            "toelichting": "geanonimiseerd",
+                            "beschikteProducten": [
+                                {
+                                    "identificatie": "123123",
+                                    "externe_identificatie": None,
+                                    "product": {
+                                        "identificatie": "12320",
+                                        "productCode": "12320",
+                                        "productsoortCode": "OVE",
+                                        "omschrijving": "autozitje",
+                                    },
+                                    "productCategorie": {
+                                        "code": "12",
+                                        "omschrijving": "Vervoervoorzieningen",
+                                    },
+                                    "resultaat": "toegewezen",
+                                    "toegewezenProduct": {
+                                        "datumIngangGeldigheid": "2012-11-30",
+                                        "datumEindeGeldigheid": None,
+                                        "datumCheck": None,
+                                        "actueel": True,
+                                        "omvang": {
+                                            "volume": 1,
+                                            "eenheid": {
+                                                "code": "82",
+                                                "omschrijving": "Stuks (output)",
+                                            },
+                                            "frequentie": {
+                                                "code": "6",
+                                                "omschrijving": "Per beschikking",
+                                            },
+                                            "omschrijving": "1 stuks (output) per beschikking",
+                                        },
+                                        "leveringsvorm": "zin",
+                                        "leverancier": {
+                                            "identificatie": "LH123123",
+                                            "agbcode": None,
+                                            "kvk": None,
+                                            "omschrijving": "Welzorg",
+                                        },
+                                        "toelichting": None,
+                                        "toewijzingen": [
+                                            {
+                                                "toewijzingNummer": "123123_OUD",
+                                                "toewijzingsDatumTijd": "2012-12-04T00:00:00",
+                                                "ingangsdatum": "2012-11-30",
+                                                "einddatum": "2017-05-31",
+                                                "datumOpdracht": "2012-12-04",
+                                                "leverancier": {
+                                                    "identificatie": "LH123",
+                                                    "agbcode": None,
+                                                    "kvk": None,
+                                                    "omschrijving": "Welzorg",
+                                                },
+                                                "referentieAanbieder": None,
+                                                "redenWijziging": {
+                                                    "code": "01",
+                                                    "omschrijving": "Administratieve correctie",
+                                                },
+                                                "leveringen": [
+                                                    {
+                                                        "begindatum": "2014-04-01",
+                                                        "einddatum": "2017-05-31",
+                                                        "redenBeeindiging": {
+                                                            "code": "04",
+                                                            "omschrijving": "Overplaatsing",
+                                                        },
+                                                    }
+                                                ],
+                                            },
+                                            {
+                                                "toewijzingNummer": "123123",
+                                                "toewijzingsDatumTijd": "2017-06-01T00:00:00",
+                                                "ingangsdatum": "2012-11-30",
+                                                "einddatum": None,
+                                                "datumOpdracht": "2017-06-01",
+                                                "leverancier": {
+                                                    "identificatie": "123123",
+                                                    "agbcode": "123123",
+                                                    "kvk": None,
+                                                    "omschrijving": "Welzorg",
+                                                },
+                                                "referentieAanbieder": None,
+                                                "redenWijziging": None,
+                                                "leveringen": [
+                                                    {
+                                                        "begindatum": "2017-06-01",
+                                                        "einddatum": None,
+                                                        "redenBeeindiging": None,
+                                                    }
+                                                ],
+                                            },
+                                        ],
+                                    },
+                                }
+                            ],
+                        },
+                        "documenten": [
+                            {
+                                "documentidentificatie": "B31242",
+                                "omschrijving": "Toekenning hulpmiddel zonder keuze (proces 0,1)",
+                                "datumDefinitief": "2012-12-04T00:00:00",
+                                "zaakidentificatie": None,
+                            }
+                        ],
+                    },
+                ]
+            }
+        }
 
 
 class ZorgnedApiMockError:
@@ -50,7 +170,24 @@ class TestAPI(FlaskServerTMATestCase):
 
         self.assertEqual(res.status_code, 200, res.data)
         self.assertEqual(res.json["status"], "OK")
-        self.assertEqual(res.json["content"], [])
+        self.assertEqual(
+            res.json["content"],
+            [
+                {
+                    "title": "autozitje",
+                    "itemTypeCode": "OVE",
+                    "dateStart": "2012-11-30",
+                    "dateEnd": None,
+                    "isActual": True,
+                    "deliveryType": "zin",
+                    "supplier": "Welzorg",
+                    "dateDecision": "2017-06-01",
+                    "serviceOrderDate": "2017-06-01",
+                    "serviceDateStart": "2017-06-01",
+                    "serviceDateEnd": None,
+                }
+            ],
+        )
 
     @patch("app.zorgned_service.requests.get", autospec=True)
     @patch("app.helpers.get_tma_certificate", lambda: server_crt)
